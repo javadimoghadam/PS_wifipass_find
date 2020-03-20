15769aa1e142d18ffe9b7af5abc910aa0896bc3b
Seyed Ali Javadi Moghadam <42797703+javadimoghadam@users.noreply.github.com>
2020-03-20 14:06:04 +0330

    Add Source File

---
+++ ShowAll Wifi with Password.ps1
@@ -0,0 +1,2 @@
+clear;
+(netsh wlan show profiles) | Select-String “\:(.+)$” | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name=”$name” key=clear)} | Select-String “Key Content\W+\:(.+)$” | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ Wifi_Name=$name;PASSWORD=$pass }} | Format-Table -AutoSize
